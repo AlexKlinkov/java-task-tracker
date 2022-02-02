d27adc4c0201b96d3c7ff197a0102e41fd810f66
Alex <klinkoffalexander@yandex.ru>
2022-02-03 00:53:16 +0300

    fix : Исправить все замечания ревьюера

---
+++ src/tracker/model/Subtask.java
@@ -0,0 +1,48 @@
+package tracker.model;
+
+public class Subtask extends Task {
+    private Epic epic; // Подзадача приходит к конкретному эпику
+    int id = 0; // идентификатор устанавливается менеджером
+
+    public Subtask(String name, String description, String status, Epic epic) {
+        super(name, description, status);
+        this.epic = epic;
+    }
+
+    // конструктор необходимый для обновления конкретной подзадачи, конкретного эпика по ID
+    public Subtask(String name, String description, String status, Epic epic, int id) {
+        super(name, description, status);
+        this.epic = epic;
+        this.id = id;
+    }
+
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Epic getEpic() {
+        return epic;
+    }
+
+    public void setEpic(Epic epic) {
+        this.epic = epic;
+    }
+
+    @Override
+    public String toString() {
+        return "Subtask{" +
+                "id=" + id +
+                ", name='" + getName() + '\'' +
+                ", description='" + getDescription() + '\'' +
+                ", status='" + getStatus() + '\'' +
+                '}';
+    }
+}
+
+
